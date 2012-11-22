540539e96065f741a5efbc3d0bbe466c463a06ef
Anshul Bajpai <bajpai.anshul@gmail.com>
2012-11-22 00:22:57 +0000

    Created gradle file and scala spike file

---
+++ src/main/scala/spike/spike.scala
@@ -0,0 +1,11 @@
+package spike
+import collection.immutable.TreeMap
+
+class Hello(greeting : String){
+  def sayHi = greeting + " World"
+}
+
+class ShoppingList(items : TreeMap[String,Int] = TreeMap.empty){
+  def addItem(name :String, count : Int) = new ShoppingList(items + (name -> count))
+  def print = items.map{e => e._1 + " " + e._2}.mkString("\n")
+}
