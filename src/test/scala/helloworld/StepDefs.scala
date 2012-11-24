abe7376285896c1f8145bb49d6844bf1613b7d23
Anshul Bajpai <bajpai.anshul@gmail.com>
2012-11-24 23:55:15 +0000

    Implemented Third Scenario - Transformation Bumped up the version of scalatest to 2.0.M5 to get Selenium Support

--- src/test/scala/helloworld/StepDefs.scala
+++ src/test/scala/helloworld/StepDefs.scala
@@ -2,5 +2,6 @@ package helloworld
 
 import cucumber.runtime.{EN, ScalaDsl}
 import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.selenium.WebBrowser
 
 trait StepDefs extends ScalaDsl with EN with ShouldMatchers
---
+++ src/test/scala/helloworld/TransformationStepDefs.scala
@@ -0,0 +1,28 @@
+package helloworld
+
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+import org.scala_tools.time.Imports._
+
+class TransformationStepDefs extends StepDefs{
+
+  val dateTimeFormat = DateTimeFormat.forPattern("MMM d, YYY")
+  var today : DateTime = null
+  var laundryDate : DateTime = null
+
+  Given("""^today is "([^"]*)"$"""){ (date:String) =>
+    today =  dateTimeFormat.parseOption(date) match {
+      case Some(d) => d
+      case None => throw new IllegalArgumentException
+    }
+  }
+
+  Given("""^I did laundry (\d+) days ago$"""){ (days:Int) =>
+       laundryDate = today - days.days
+  }
+
+  Then("""^my laundry day must have been "([^"]*)"$"""){ (laundryDate:String) =>
+      this.laundryDate.toString(dateTimeFormat) should equal(laundryDate)
+  }
+
+}
